4c9ea7c97d08b1e7fe4ddcfb9e7fcfe138287759
Fe-Nik-S <mikifeynman@gmail.com>
2024-04-06 21:03:32 +0300

    Added first simple component

--- learning/js/react-quick-v1/app/js/app.js
+++ learning/js/react-quick-v1/app/js/app.js
@@ -2,7 +2,17 @@
 console.log(React);
 console.log(ReactDOM);
 
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Hi, Here is a first react component!
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hi, World!</h1>,
+    <App />,
     document.getElementById('root')
 );
