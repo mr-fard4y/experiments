ff36109ba377382e81e2850d4affe84fb4f0b2fc
Fe-Nik-S <mikifeynman@gmail.com>
2024-04-06 21:22:51 +0300

    Added app basic components

--- learning/js/react-quick-v1/app/js/app.js
+++ learning/js/react-quick-v1/app/js/app.js
@@ -6,11 +6,33 @@ var App = React.createClass({
     render: function() {
         return (
             <div className="app">
-                Hi, Here is a first react component!
+                First app
+                <News />
+                <Comments />
             </div>
         );
     }
 });
+
+var News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                News: Nothing to show.
+            </div>
+        )
+    }
+})
+
+var Comments = React.createClass({
+    render: function() {
+        return (
+            <div className="comments">
+                Comments: Nothing to show.
+            </div>
+        )
+    }
+})
 
 ReactDOM.render(
     <App />,
