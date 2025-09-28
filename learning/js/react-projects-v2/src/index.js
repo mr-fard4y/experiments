15de5690d42059df9ec906fb2d0aae1ff54b42bc
Fe-Nik-S <mikifeynman@gmail.com>
2025-09-28 21:44:28 +0500

    JS. React projects. Inited react base skeleton

--- learning/js/react-projects-v2/src/index.js
+++ learning/js/react-projects-v2/src/index.js
@@ -1,2 +1,11 @@
 
-console.log('Hello world');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Hello world</h1>;
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
